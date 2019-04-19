7f8b6c2af3f41dfacb53f4161ac9aa3773ebefaa
lululee123 <lulululu123456123456@gmail.com>
2019-04-19 12:50:46 +0800

    edit calculate

--- src/components/js/CssSizeInfo.js
+++ src/components/js/CssSizeInfo.js
@@ -33,21 +33,41 @@ class CssSizeInfo extends Component {
     if (this.props.showInfo){
       return (
         <div className="cssInfoBox">
-          <div className="cssInfoBox__input">
+          {/* <div className="cssInfoBox__input">
             <input placeholder="縮放界線(1920)" onChange={ this.setWeb} />
             <input placeholder="縮放界線(1024)" onChange={ this.setMobile} />
-          </div>
-          <div className="cssInfoBox__max">
-            <div>width: { this.props.imageInfo.width }px</div>
-            <div>height: { this.props.imageInfo.height }px</div>
-          </div>
-          <div className="cssInfoBox__web">
-            <div>width: { ( ( this.props.imageInfo.width / this.state.web ) * 100 ).toFixed(2) }vw</div>
-            <div>height: { ( ( this.props.imageInfo.height / this.state.web ) * 100).toFixed(2) }vw</div>
-          </div>
-          <div className="cssInfoBox__mobile">
-            <div>width: 100vw</div>
-            <div>height: { ( ( this.props.imageInfo.height / this.props.imageInfo.width ) * 100 ).toFixed(2) }vw</div>
+          </div> */}
+          <div className="cssInfoBox__info">
+            <div className="cssInfoBox__info_main">
+              主圖：
+              <div className="cssInfoBox__max">
+                <div>width: { this.props.imageInfo.width }px</div>
+                <div>height: { this.props.imageInfo.height }px</div>
+              </div>
+              <div className="cssInfoBox__web">
+                <div>width: 100vw</div>
+                <div>height: { ( ( this.props.imageInfo.height / this.props.imageInfo.width ) * 100).toFixed(2) }vw</div>
+              </div>
+              <div className="cssInfoBox__mobile">
+                <div>width: 100vw</div>
+                <div>height: { ( ( this.props.imageInfo.height / this.props.imageInfo.width ) * 100 ).toFixed(2) }vw</div>
+              </div>
+            </div>
+            <div className="cssInfoBox__info_normal">
+              元件：
+              <div className="cssInfoBox__max">
+                <div>width: { this.props.imageInfo.width }px</div>
+                <div>height: { this.props.imageInfo.height }px</div>
+              </div>
+              <div className="cssInfoBox__web">
+                <div>width: { ( ( this.props.imageInfo.width / this.state.web ) * 100 ).toFixed(2) }vw</div>
+                <div>height: { ( ( this.props.imageInfo.height / this.state.web ) * 100).toFixed(2) }vw</div>
+              </div>
+              <div className="cssInfoBox__mobile">
+                <div>width: { ( ( this.props.imageInfo.width / this.state.mobile ) * 100 ).toFixed(2) }vw</div>
+                <div>height: { ( ( this.props.imageInfo.height / this.state.mobile ) * 100 ).toFixed(2) }vw</div>
+              </div>
+            </div>
           </div>
         </div>
       );
